1ade53e6b5d4da0db352a680ff428d7e6223bf19
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-01-24 22:30:23 -0500

    Start Pause Menu

---
+++ Modules/UI/PauseScreen/pause_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var paused := false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("Pause"):
+		if !paused:
+			get_tree().paused = true
+			show()
+			paused = true
+		else:
+			hide()
+			get_tree().paused = false
+			paused = false
