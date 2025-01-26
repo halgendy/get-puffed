400422c3c34c78946a71ebae97bfc942f69e3872
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-01-26 12:53:23 -0500

    More Audio Effects

--- modules/health_pickup/health_pickup.gd
+++ modules/health_pickup/health_pickup.gd
@@ -2,11 +2,13 @@ extends Node3D
 
 signal picked_up
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+const JUMP_IN_PUDDLE = preload("res://sfx/JumpInPuddle.wav")
 
 func _ready() -> void:
 	animation_player.play("idle")
 func _on_pickup_box_body_entered(body: Node3D) -> void:
 	if body is Player:
+		AudioManager.play_audio(JUMP_IN_PUDDLE)
 		picked_up.emit()
 		body.health.fill(30)
 		queue_free()
