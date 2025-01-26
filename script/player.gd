fe520392307e3a074091cfdf7a34a760c7aa830d
isaacspool <spool.i@northeastern.edu>
2025-01-26 00:14:46 -0500

    Added backroom, fixed some materials in the front room

--- script/player.gd
+++ script/player.gd
@@ -44,6 +44,7 @@ var pitch_input := 0.0
 # Called when the node enters the scene tree for the **first time.**
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	$AnimationPlayer.play("walk")
 
 
 func respawn():
--- script/world.gd
+++ script/world.gd
@@ -1,14 +1,5 @@
+@tool
 extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_area_3d_body_entered(body):
@@ -16,3 +7,12 @@ func _on_area_3d_body_entered(body):
 		print("you win")
 		await get_tree().create_timer(0.1).timeout
 		get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _ready():
+	var front_room = $"front room with collisions"
+	var nodes = [$"WholeScene (5)", front_room.get_node("display"), front_room.get_node("display1"), front_room.get_node("group7")]
+	for node in nodes:
+		for light in node.get_children():
+			if light is Light3D:
+				light.light_energy = 0.3
