7699161e6beb7e8fcafb9c56d436189a5f7fc22c
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2025-01-26 14:31:05 -0500

    Added Music

--- script/world.gd
+++ script/world.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 func _on_area_3d_body_entered(body):
 	if body is Player:
+		AudioManager.stop_music()
 		print("you win")
 		await get_tree().create_timer(0.1).timeout
 		get_tree().change_scene_to_file("res://main.tscn")
